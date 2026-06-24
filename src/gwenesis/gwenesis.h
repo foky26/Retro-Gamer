7f172ed4879fc5d67e6813eee0f49935c224f8a4
CGR <137554397+foky26@users.noreply.github.com>
2026-06-24 19:32:28 +0200

    Add files via upload

---
+++ src/gwenesis/gwenesis.h
@@ -0,0 +1,9 @@
+/* gwenesis.h - adapted for Arduino flat include */
+#include "m68k.h"
+#include "z80inst.h"
+#include "ym2612.h"
+#include "gwenesis_bus.h"
+#include "gwenesis_io.h"
+#include "gwenesis_vdp.h"
+#include "gwenesis_savestate.h"
+#include "gwenesis_sn76489.h"
